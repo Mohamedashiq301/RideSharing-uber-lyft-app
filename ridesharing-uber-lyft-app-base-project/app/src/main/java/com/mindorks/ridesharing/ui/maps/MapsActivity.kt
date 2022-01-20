89faa115009086b9d3ab7a7ccd8aa509b4d245fe
Mohamedashiq301 <mohamedashiq301@gmail.com>
2022-01-20 19:44:00 +0530

    Add some files

--- ridesharing-uber-lyft-app-base-project/app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
+++ ridesharing-uber-lyft-app-base-project/app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
@@ -6,10 +6,17 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.mindorks.ridesharing.R
+import com.mindorks.ridesharing.data.network.NetworkService
+import com.mindorks.ridesharing.utils.PermissionUtils
 import com.mindorks.ridesharing.utils.ViewUtils
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
 
+    companion object{
+        private const val TAG="MapsActivity"
+        private const val LOCATION_PERMISSION_REQUEST_CODE=999
+    }
+    private lateinit var presenter: MapsPresenter
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +26,34 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        presenter= MapsPresenter(NetworkService())
+        presenter.onAttach(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+    }
+
+    override fun onStart() {
+        super.onStart()
+        when{
+            PermissionUtils.isAccessFineLocationGranted(this)->{
+                when{
+                    PermissionUtils.isLocationEnabled(this)->{
+                        //fetch the location
+                    }else->{
+                        PermissionUtils.showGPSNotEnabledDialog(this)
+                    }
+                }
+            }else->{
+                PermissionUtils.requestAccessFineLocationPermission(this,
+                    LOCATION_PERMISSION_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        presenter.onDetach()
+        super.onDestroy()
     }
 }
