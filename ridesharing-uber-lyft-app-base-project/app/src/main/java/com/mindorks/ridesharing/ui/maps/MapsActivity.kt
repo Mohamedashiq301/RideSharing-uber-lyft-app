9675513dd312745656d7c83f28f47743660d600b
Mohamedashiq301 <mohamedashiq301@gmail.com>
2022-01-21 22:43:57 +0530

    Add some files

--- ridesharing-uber-lyft-app-base-project/app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
+++ ridesharing-uber-lyft-app-base-project/app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
@@ -1,23 +1,33 @@
 package com.mindorks.ridesharing.ui.maps
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.mindorks.ridesharing.R
 import com.mindorks.ridesharing.data.network.NetworkService
 import com.mindorks.ridesharing.utils.PermissionUtils
 import com.mindorks.ridesharing.utils.ViewUtils
 
-class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
 
-    companion object{
-        private const val TAG="MapsActivity"
-        private const val LOCATION_PERMISSION_REQUEST_CODE=999
+    companion object {
+        private const val TAG = "MapsActivity"
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 999
     }
+
     private lateinit var presenter: MapsPresenter
     private lateinit var mMap: GoogleMap
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private lateinit var locationCallback: LocationCallback
+    private var currentLatLng: LatLng? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +36,14 @@ class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        presenter= MapsPresenter(NetworkService())
+        presenter = MapsPresenter(NetworkService())
         presenter.onAttach(this)
+    }
+
+    private fun setUpLocationListener(){
+        fusedLocationProviderClient= FusedLocationProviderClient(this)
+        val locationRequest=LocationRequest().setInterval(2000).setFastestInterval(2000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -36,18 +52,47 @@ class MapsActivity : AppCompatActivity(),MapsView, OnMapReadyCallback {
 
     override fun onStart() {
         super.onStart()
-        when{
-            PermissionUtils.isAccessFineLocationGranted(this)->{
-                when{
-                    PermissionUtils.isLocationEnabled(this)->{
+        when {
+            PermissionUtils.isAccessFineLocationGranted(this) -> {
+                when {
+                    PermissionUtils.isLocationEnabled(this) -> {
                         //fetch the location
-                    }else->{
+                    }
+                    else -> {
                         PermissionUtils.showGPSNotEnabledDialog(this)
                     }
                 }
-            }else->{
-                PermissionUtils.requestAccessFineLocationPermission(this,
-                    LOCATION_PERMISSION_REQUEST_CODE)
+            }
+            else -> {
+                PermissionUtils.requestAccessFineLocationPermission(
+                    this,
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    when {
+                        PermissionUtils.isLocationEnabled(this) -> {
+                            //fetch the location
+                        }
+                        else -> {
+                            PermissionUtils.showGPSNotEnabledDialog(this)
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Location permission not granted", Toast.LENGTH_LONG)
+                        .show()
+                }
             }
         }
     }
